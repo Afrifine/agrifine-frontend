1e848d20a77037a023552ef2d094f95c9a8d83c4
Nicholas Mutale <nicomutale@gmail.com>
2022-08-21 16:46:49 +0200

    Import confrim login register and export them

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Home from "./home/Home";
 import Products from "./products/Products";
 import About from "./about/About";
-
-export { Home, Products, About };
+import Confirm from "./auth/Confirm";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+export { Home, Products, About, Confirm, Login, Register };
